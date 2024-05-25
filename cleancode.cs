70921ea0a746f534e97a772eb85a87c02d45d541
EmersunD <emersuncampos@outlook.com>
2024-05-25 16:52:06 -0300

    first commit

---
+++ cleancode.cs
@@ -0,0 +1,25 @@
+﻿
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+
+
+//or
+
+string pessoa = "Bob";
+int num = 3;
+double temperatura = 34.4;
+
+Console.WriteLine($"Hello,{pessoa}! You have {num} in your inbox. The temperature is {temperatura} ");
+
+Console.ReadLine();
+
+//This a same code, but, second is a clean code.
