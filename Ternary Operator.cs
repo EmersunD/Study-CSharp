a35a61727e0b9057c6ed4863449a38eb253cc3a7
EmersunD <emersuncampos@outlook.com>
2025-01-09 21:51:07 -0300

    Adicionado exemplo de operador ternário em C#

---
+++ Ternary Operator.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ternary Operator\n");
+
+int value1 = 1;
+
+Console.WriteLine($"O valor positivo é de {value1}, e o valor negativo é de {-value1}");
+
+//Condicional "?"
+
+Console.WriteLine("Informa a temperatura\n");
+
+var temp = Convert.ToDouble(Console.ReadLine());
+
+var resultado = temp > 27 ? "Quente" : "Frio";
+
+Console.WriteLine($"A temperatura esta {resultado}");
+
+
+
+
